c906afa801f5f10611c4e07a93f61746937f43ec
JoonasS94 <sariola94@gmail.com>
2025-01-24 20:11:31 +0200

    Shooting bubbles functionality
    Bubbles are fired from tip of player one's gun when pressing RT (R2) trigger. There is small 0,3 seconds cooldown for firing bubbles.

--- Assets/Scripts/PlayerOneGunController.cs
+++ Assets/Scripts/PlayerOneGunController.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerOneGunController : MonoBehaviour
 {
     public Transform weapon; // Ase, jota liikutetaan
+    public bool PlayerOneFiringCooldown = false;
+    public GameObject PlayerOneShotBubblePrefab;
+    public Transform PlayerOneWeaponMuzzle;
+    private float ShotPower = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,5 +31,42 @@ public class PlayerOneGunController : MonoBehaviour
             // Aseta aseen rotaatio välittömästi
             weapon.rotation = Quaternion.Euler(0, 0, angle);
         }
+
+
+
+
+        // Lue RT (R2)-arvo
+        float rightTrigger = Input.GetAxis("RightTrigger");
+
+        // Tarkista, painetaanko RT:tä
+        if (rightTrigger > 0.1f)
+        {
+            Debug.Log("RT painettu, arvo: " + rightTrigger);
+            FirePlayerOneWeaponBubble();
+        }
+    }
+
+    void FirePlayerOneWeaponBubble()
+    {
+        if (PlayerOneFiringCooldown == false)
+        {
+            PlayerOneFiringCooldown = true;
+            StartCoroutine(PlayerOneFiringCooldownTimer());
+
+            // Instansioi kupla aseen piipusta ja huomioi aseen rotaatio
+            GameObject createdPlayerOneBubble = Instantiate(PlayerOneShotBubblePrefab, PlayerOneWeaponMuzzle.position, PlayerOneWeaponMuzzle.rotation);
+
+            // Kuplan Rb
+            Rigidbody PlayerOneBubbleRigidbody = createdPlayerOneBubble.GetComponent<Rigidbody>();
+
+            // Kuplaan energia jolla lähtee liikkeelle aseen piipusta eteenpain
+            PlayerOneBubbleRigidbody.AddForce(PlayerOneWeaponMuzzle.up * ShotPower, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PlayerOneFiringCooldownTimer()
+    {
+        yield return new WaitForSeconds(0.3f);
+        PlayerOneFiringCooldown = false;
     }
 }
